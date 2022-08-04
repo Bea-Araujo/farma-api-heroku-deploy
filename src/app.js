881c61d700e56574c785463262d461927eca321e
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-08-04 20:52:08 -0300

    gadi was here

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const sequelize = require("./config/dbcontext");
 const Routes = require("./routes/App.routes")
 
+let port = process.env.PORT || 3030;
+
 const app = express();
 
 sequelize.sync().then(() => console.log("database connect Successfully!!!"));
@@ -11,6 +13,6 @@ app.use(express.json());
 
 app.use("/", Routes);
 
-app.listen(3030, () => {
-    console.log("Server started on port 3030");
+app.listen(path, () => {
+    console.log(`Server started on port ${port}`);
 })
