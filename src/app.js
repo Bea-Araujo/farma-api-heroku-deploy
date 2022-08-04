6e786eca1a630a2661a3f092f12c20422e692099
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-08-04 20:54:43 -0300

    gadi was here

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 // import express from 'express'
 const express = require("express");
+const cors = require('cors')
 const sequelize = require("./config/dbcontext");
 const Routes = require("./routes/App.routes")
 
 let port = process.env.PORT || 3030;
 
 const app = express();
+app.use(cors())
 
 sequelize.sync().then(() => console.log("database connect Successfully!!!"));
 
