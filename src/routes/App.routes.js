3b06549ba4b22c2b31db91f933190fcd97459c3f
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-08-04 21:41:25 -0300

    gadi was here

--- src/routes/App.routes.js
+++ src/routes/App.routes.js
@@ -1,4 +1,4 @@
-const express = require ("express");
+const express = require("express");
 const ProductController = require("../controller/ProductController");
 const SupplierController = require("../controller/SupplierController")
 const CategoryController = require("../controller/CategoryController");
@@ -8,29 +8,29 @@ const router = express.Router();
 
 // Routes for the Product
 router.route("/product")
-.get(ProductController.allProduct)
-.post(ProductController.createProduct);
+    .get(ProductController.allProduct)
+    .post(ProductController.createProduct);
 
 router.route("/product/:id")
-.get(ProductController.oneProduct)
-.put(ProductController.UpdateProduct)
-.delete(ProductController.deleteProduct)
+    .get(ProductController.oneProduct)
+    .put(ProductController.UpdateProduct)
+    .delete(ProductController.deleteProduct)
 
 // Routes the of Category
 router.route("/category")
-.get(CategoryController.allCategory)
-.post(CategoryController.createCategory);
+    .get(CategoryController.allCategory)
+    .post(CategoryController.createCategory);
 
 router.route("/category/:id")
-.get(CategoryController.oneCategory)
-.put(CategoryController.UpdateCategory)
-.delete(CategoryController.deleteCategory)
+    .get(CategoryController.oneCategory)
+    .put(CategoryController.UpdateCategory)
+    .delete(CategoryController.deleteCategory)
 
 //Routes for the Supplier
 router.route("/supplier")
     .post(SupplierController.createSupplier)
     .get(SupplierController.getAllSuppliers);
-    
+
 router.route("/supplier/:id")
     .get(SupplierController.getSupplier)
     .put(SupplierController.updateSupplier)
@@ -38,24 +38,24 @@ router.route("/supplier/:id")
 
 // Routes of Customer
 router.route("/customer")
-.post(CustomerController.createCustomer)
-.get(CustomerController.getAllCustomer);
+    .post(CustomerController.createCustomer)
+    .get(CustomerController.getAllCustomer);
 
 router.route("/customer/:id")
-.get(CustomerController.getCustomer)
-.put(CustomerController.updateCustomer)
-.delete(CustomerController.deleteCustomer);
+    .get(CustomerController.getCustomer)
+    .put(CustomerController.updateCustomer)
+    .delete(CustomerController.deleteCustomer);
 
 
 // Routs of Pharmacist
 router.route("/pharmacist")
-.get(PharmacistController.allPharmacist)
-.post(PharmacistController.createPharmacist);
+    .get(PharmacistController.allPharmacist)
+    .post(PharmacistController.createPharmacist);
 
 router.route("/pharmacist/:id")
-.get(PharmacistController.onePharmacist)
-.put(PharmacistController.updatePharmacist)
-.delete(PharmacistController.deletePharmacist);
+    .get(PharmacistController.onePharmacist)
+    .put(PharmacistController.updatePharmacist)
+    .delete(PharmacistController.deletePharmacist);
 
 
 module.exports = router;
